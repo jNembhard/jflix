40b24e2f1d600db9ad9836d45ee18ab21fa3e743
jNembhard <nembhardjl@outlook.com>
2021-07-05 02:08:43 -0400

    Linked to youtube videos and fixed styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function Nav() {
-    useEffect()=> {
-        
-    }
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      // actives scroll at specific y-axis scroll value
+      if (window.scrollY > 1) {
+        setShow(true);
+      } else setShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav-black"}`}>
       <img
         className="nav-logo"
         src={process.env.PUBLIC_URL + "/images/JFlix.png"}
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,23 +1,46 @@
 import React, { useState, useEffect } from "react";
 import axios from "../axios";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const baseImageUrl = "http://image.tmdb.org/t/p/original/";
 
 function Row(props) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     // if [], run once when the row loads, and don't run again
     async function getData() {
       const request = await axios.get(props.getUrl);
-      // combines baseUrl with API portions
       setMovies(request.data.results);
       return request;
     }
     getData();
   }, [props.getUrl]); // array dependent in async function
 
-  // console.log(movies);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          // https://www.youtube.com/watch?v={someTrailerValue}
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((err) => console.log(err));
+    }
+  };
 
   return (
     <div className="row">
@@ -26,6 +49,7 @@ function Row(props) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row-poster ${props.isLargeRow && "row-posterLarge"}`}
             src={`${baseImageUrl}${
               props.isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -35,6 +59,7 @@ function Row(props) {
           />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
